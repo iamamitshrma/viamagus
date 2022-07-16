3ded9a187e0077c78728f6b0249d67039a86deb6
Amit Sharma <iamamitshrma@gmail.com>
2022-07-16 22:46:44 +0530

    date

--- src/pages/Date.jsx
+++ src/pages/Date.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react'
+import moment from 'moment';
+import React, { useEffect, useState } from 'react'
+import './date.css';
 
 const DatePage = () => {
   const [fromDate, setFromDate] = useState(new Date());
@@ -13,20 +15,26 @@ const DatePage = () => {
 
   const durationHandler = (e) => {
     setDuration(e.target.value);
-    const cal = calculateDate(duration, fromDate);
-    console.log(cal);
+    // const cal = calculateDate(Number(duration), new Date(fromDate));
+    // setToDate(cal);
   }
 
+  useEffect(() => {
+    const cal = calculateDate(Number(duration), new Date(fromDate));
+    setToDate(cal);
+  }, [duration])
   return (
-    <div>
+    <div className='date--container'>
+      <div className="center">
         <label htmlFor="birthdaytime">Date From:</label>
         <input onChange={(e)=>setFromDate(e.target.value)} type="datetime-local" id="birthdaytime" name="time" value={fromDate}></input>
 
-        <label htmlFor="duration">Duration</label>
-        <input onChange={durationHandler} type="number" name="duration" id="duration" value={duration}/>
+        <label htmlFor="duration">Duration(in hours)</label>
+        <input onChange={durationHandler} type="number" name="duration" id="duration" value={duration} placeholder="Enter duration in hours"/>
 
         <label htmlFor="Totime">Date To:</label>
-        <input type="datetime-local" id="Totime" name="time" value={toDate} ></input>
+        <input type="datetime-local" id="Totime" name="time" value={moment(toDate).format('YYYY-MM-DDTHH:mm')}></input>
+      </div>
     </div>
   )
 }
